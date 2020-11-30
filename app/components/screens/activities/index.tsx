75dd7a577093d2068c144920e68fe7c7e8d8043d
yuqiwang <yuqiwang>
2020-12-01 00:56:17 +0800

    init activities

---
+++ app/components/screens/activities/index.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+/* React Imports */
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, Image } from 'react-native';
+
+/* React Paper Imports */
+import { Provider as PaperProvider, Button } from 'react-native-paper';
+
+/* Theming */
+import Theme from '../../../../styles/theme.style';
+import ButtonStyle from '../../../../styles/button.style';
+import { ContainedButtonPaperTheme } from '../../../../styles/paper.style';
+
+/* Screens */
+import ShowActivityCategories from './view_activities/ShowActivityCategories';
+import ActivitySetupFormView from './setup_activities/ActivitySetupFormView'
+
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+export function Activities(props: any) {
+
+    // Get Naviation Object.
+    const navigation = props.navigation;
+
+    return (
+        <PaperProvider>
+            <View style={styles.container}>
+                <KeyboardAvoidingView style={styles.innerContainer}
+                    behavior={Platform.OS == "ios" ? "padding" : "height"}>
+                    <View style={styles.btnContainer}>
+                        <Button icon="creation"
+                            style={[ButtonStyle.btn,
+                            {
+                                justifyContent: 'center',
+                                marginLeft: 5,
+                                flex: 1
+                            }]}
+                            theme={ContainedButtonPaperTheme}
+                            labelStyle={{
+                                color: Theme.primary,
+                                fontWeight: '700',
+                                fontSize: 14,
+                                letterSpacing: 2
+                            }}
+                            mode="contained"
+                            onPress={() => navigation.navigate('ShowActivityCategories')}>See All Activities</Button>
+                        <Button icon="creation"
+                            style={[ButtonStyle.btn,
+                            {
+                                justifyContent: 'center',
+                                marginLeft: 5,
+                                flex: 1
+                            }]}
+                            theme={ContainedButtonPaperTheme}
+                            labelStyle={{
+                                color: Theme.primary,
+                                fontWeight: '700',
+                                fontSize: 14,
+                                letterSpacing: 2
+                            }}
+                            mode="contained"
+                            onPress={() => navigation.navigate('ActivitySetupFormView')}>Setup An Activity</Button>
+                    </View>
+                </KeyboardAvoidingView>
+            </View>
+        </PaperProvider>
+    );
+}
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Theme.white,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    innerContainer: {
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    btnContainer: {
+        flexDirection: 'row',
+        width: '80%',
+        paddingTop: 20,
+        paddingBottom: 20
+    }
+});
+
+
+export default Activities;
