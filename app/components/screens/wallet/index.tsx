d26783321406ea2938b672e0fb6f06c35a596a53
Ye Wang <kyewang@ucdavis.edu>
2020-11-30 22:16:44 +0800

    done wallet page

--- app/components/screens/wallet/index.tsx
+++ app/components/screens/wallet/index.tsx
@@ -3,13 +3,25 @@ import { SafeAreaView, Text, View, Image, ActivityIndicator } from 'react-native
 import { StatusBar, ScrollView, StyleSheet } from 'react-native';
 
 import Theme from '../../../../styles/theme.style';
-import Spacing from '../../../../styles/spacing.style';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import currency from 'currency.js';
 
 /* Components */
 import Header from '../../common/header';
-import Card from '../../common/card';
+import TransCard from '../../common/transCard';
+
 
 function Wallet(props: any) {
+    const [balance, setBalance] = React.useState("null");
+
+    // API Call: User Details and Upcoming Activities.
+    useEffect(() => {
+        AsyncStorage.getItem('email').then(email => {
+            if (email != null) getBalance(email, setBalance);
+        })
+    }, [])
+
     return (
         <View>
             <View style={{ backgroundColor: Theme.primary }}>
@@ -18,28 +30,77 @@ function Wallet(props: any) {
                 </SafeAreaView>
             </View>
 
-            <View style={[styles.card, Spacing.mt1]}>
+            <View style={[styles.card, styles.myspace]}>
                 <View style={styles.WalletSect}>
-                    <Text style={{
-                            fontSize: 20,
-                            }}>
-                        Wallet</Text>
-                    <Text style={styles.balance}>$52.50</Text>
+                    <Text style={{fontSize: 20,}}>Wallet</Text>
+                    <Text style={styles.mybalance}>{currency(balance).format()}</Text>
                 </View>
             </View>
 
-            <View style={{ flexDirection: 'row' }}>
+            <View style={{
+                flexDirection: 'row',
+                marginLeft: 20,
+                marginTop: 20,}}>
                         <Text style={styles.title}>PREVIOUS TRANSACTIONS</Text>
                         <Text style={styles.titleAlt}>💵</Text>
             </View>
+            <ScrollView style={styles.scrollView}>
+                <TransCard 
+                    trans={5}
+                    date='14/12/18'/>
+                <TransCard 
+                    trans = {2}
+                    date = '11/11/18'
+                />
+                <TransCard 
+                    trans={-2}
+                    date='18/11/18'/>
+                <TransCard 
+                    trans = {-7.3}
+                    date = '13/17/20'
+                />
+                <TransCard 
+                    trans={5}
+                    date='14/12/18'/>
+                <TransCard 
+                    trans = {2}
+                    date = '11/11/18'
+                />
+                <TransCard 
+                    trans={-2}
+                    date='18/11/18'/>
+                <TransCard 
+                    trans = {-7.3}
+                    date = '13/17/20'
+                />
+            </ScrollView>
         </View>
     )
 }
 
+function getBalance(email: any, setBalance: any): void {
+    fetch(`https://z3kx6gvst6.execute-api.us-east-2.amazonaws.com/dev/user-details/${email}`, { method: 'GET' }).then(response => response.json()).then(res => {
+        setBalance(res['balance'])
+    })
+}
+
 const styles = StyleSheet.create({
+    myicon: {
+        fontSize: 30,
+    },
+    myspace: {
+        margin: 20,
+    },
+    transText1: {
+        fontSize: 20,
+        fontWeight: '600',
+    },
+    transText2: {
+        fontSize: 13,
+    },
     scrollView: {
-        backgroundColor: Theme.white,
-        paddingHorizontal: 20,
+        // backgroundColor: Theme.white,
+        // paddingHorizontal: 20,
         paddingTop: 20,
         paddingBottom: 20,
         marginBottom: 80
@@ -77,7 +138,7 @@ const styles = StyleSheet.create({
         color: Theme.black,
         textTransform: 'uppercase'
     },
-    balance: {
+    mybalance: {
         fontSize: 40,
         fontWeight: '600',
     },
