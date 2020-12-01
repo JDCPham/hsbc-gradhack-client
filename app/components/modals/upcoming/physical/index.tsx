080b95c3455deb1b2e5ee46605c9df9c7421f559
--replace-all <jdcpham@outlook.com>
2020-12-01 11:36:21 +0000

    Update Merge

--- app/components/modals/upcoming/physical/index.tsx
+++ app/components/modals/upcoming/physical/index.tsx
@@ -12,6 +12,7 @@ import { Button } from 'react-native-paper';
 import { ContainedButtonPaperTheme } from '../../../../../styles/paper.style';
 import { OutlinedButtonPaperTheme } from '../../../../../styles/paper.style';
 import currency from 'currency.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function UpcomingPhysicalActivity(props: any) {
 
@@ -32,8 +33,8 @@ function UpcomingPhysicalActivity(props: any) {
 
 
     if (isLoading) return (
-        <View style={[styles.container, {justifyContent: 'center', alignItems: 'center'}]}>
-            <ActivityIndicator size="large" color={Theme.black}/>
+        <View style={[styles.container, { justifyContent: 'center', alignItems: 'center' }]}>
+            <ActivityIndicator size="large" color={Theme.black} />
         </View>
     )
     else return (
@@ -51,6 +52,7 @@ function UpcomingPhysicalActivity(props: any) {
                         <Image source={{ uri: image }} style={[styles.image]} />
                         <Text style={styles.subtitle}>{location}</Text>
                     </View>
+                    <Button icon="creation" onPress={() => props.setModalVisible(false)} style={[ButtonStyle.btn, { justifyContent: 'center', flex: 1, marginHorizontal: 20, marginTop: 50, borderColor: Theme.black, borderWidth: 2 }]} theme={OutlinedButtonPaperTheme} labelStyle={{ color: Theme.black, fontWeight: '700', fontSize: 14, letterSpacing: 2 }} mode="outlined">Back to dashboard</Button>
                     <View style={{ paddingHorizontal: 20, marginTop: 40 }}>
                         <View style={{ flexDirection: 'row' }}>
                             <Text style={[styles.heading]}>DATE</Text>
@@ -102,9 +104,16 @@ function UpcomingPhysicalActivity(props: any) {
                     </View>
 
                     <Button icon="creation" onPress={() => props.setModalVisible(false)} style={[ButtonStyle.btn, { justifyContent: 'center', flex: 1, marginHorizontal: 20, marginTop: 50, borderColor: Theme.black, borderWidth: 2 }]} theme={OutlinedButtonPaperTheme} labelStyle={{ color: Theme.black, fontWeight: '700', fontSize: 14, letterSpacing: 2 }} mode="outlined">Back to dashboard</Button>
-                    <Button icon="creation" onPress={() => props.setModalVisible(false)} style={[ButtonStyle.btn, { justifyContent: 'center', flex: 1, marginHorizontal: 20, marginTop: 10, borderColor: Theme.black, borderWidth: 2 }]} theme={OutlinedButtonPaperTheme} labelStyle={{ color: Theme.primary, fontWeight: '700', fontSize: 14, letterSpacing: 2 }} mode="contained">Cancel Attendance</Button>
-                    <Button icon="creation" onPress={() => props.setModalVisible(false)} style={[ButtonStyle.btn, { justifyContent: 'center', flex: 1, marginHorizontal: 20, marginTop: 10, borderColor: Theme.primary, borderWidth: 2, backgroundColor: Theme.primary }]} theme={ContainedButtonPaperTheme} labelStyle={{ color: Theme.black, fontWeight: '700', fontSize: 14, letterSpacing: 2 }} mode="contained">Sign in to event</Button>
-                    <View style={{ marginVertical: 50 }}></View>
+                    
+                    <Button icon="creation" onPress={() => props.setModalVisible(false)} style={[ButtonStyle.btn, { justifyContent: 'center', flex: 1, marginHorizontal: 20, marginTop: 20, borderColor: Theme.primary, borderWidth: 2, backgroundColor: Theme.primary }]} theme={ContainedButtonPaperTheme} labelStyle={{ color: Theme.black, fontWeight: '700', fontSize: 14, letterSpacing: 2 }} mode="contained">Sign in to event</Button>
+                    <View style={{marginTop: 10}}>
+                        <Button icon="creation" style={[ButtonStyle.btn, { justifyContent: 'center', flex: 1, marginHorizontal: 20, marginTop: 10, borderColor: Theme.black, borderWidth: 2 }]} theme={OutlinedButtonPaperTheme} labelStyle={{ color: Theme.primary, fontWeight: '700', fontSize: 14, letterSpacing: 2 }} mode="contained" onPress={() => cancelAttendance(props.activity, props.setModalVisible)}>Cancel Attendance</Button>
+                        <View style={{width: '100%', alignItems: 'flex-end', paddingRight: 20, marginTop: 5}}>
+                            <Text style={{fontWeight: '600', color: 'red'}}>Cancellation incurs $5.00 penalty fee.</Text>
+
+                        </View>
+                    </View>
+                    <View style={{ marginVertical: 60 }}></View>
                 </ScrollView>
             </View>
 
@@ -124,6 +133,15 @@ function getData(identifier: any, setIsLoading: any, setImage: any, setTitle: an
         setPenaltyFee(res['Penalty'])
         setIsLoading(false)
         console.log(res)
+    })
+}
+
+function cancelAttendance(identifier: any, setModalVisible: any): void {
+    if (identifier == null) return;
+    AsyncStorage.getItem('email').then(email => {
+        fetch(`https://z3kx6gvst6.execute-api.us-east-2.amazonaws.com/dev/delete-activity/${email}/${identifier}`, { method: 'DELETE' }).then(response => response.json()).then(res => {
+            setModalVisible(false);
+        })
     })
 }
 
